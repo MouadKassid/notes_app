50cd330475ce55f00fb32f5cab3deca3377e7350
MouadKassid <kassid501@gmail.com>
2025-12-05 18:31:13 +0100

    show model bottom sheet

---
+++ lib/views/widgets/add_note_buttom_sheet.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddnoteButtomSheet extends StatelessWidget {
+  const AddnoteButtomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/views/widgets/notes_list_view.dart
+++ lib/views/widgets/notes_list_view.dart
@@ -5,13 +5,17 @@ class NoteslistView extends StatelessWidget {
   const NoteslistView({super.key});
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return const Padding(
-          padding: EdgeInsets.symmetric(vertical: 4),
-          child: NoteItem(),
-        );
-      },
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: ListView.builder(
+        padding: EdgeInsets.zero,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(vertical: 4),
+            child: NoteItem(),
+          );
+        },
+      ),
     );
   }
 }
